4cce2e404abb9998301ba431947cfb9ad44a6dca
Tom Mumford <thm37@aber.ac.uk>
2023-04-18 16:53:03 +0100

    Added a top app bar

--- app/src/main/java/com/mumford/bookervatcalculator/MainActivity.kt
+++ app/src/main/java/com/mumford/bookervatcalculator/MainActivity.kt
@@ -1,24 +1,45 @@
 package com.mumford.bookervatcalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import com.mumford.bookervatcalculator.ui.theme.BookerVATCalculatorTheme
+import androidx.compose.ui.unit.dp
+import com.example.compose.BookerVATCalculatorTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookerVATCalculatorTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    ItemList()
+                    Scaffold(
+                        topBar =
+                        {
+                            TopAppBar(
+                                title = { Text("Booker VAT Calculator") }
+                            )
+                        }) {
+                        Box(
+                            Modifier.padding(top = 56.dp) // Adjust the value as needed
+                        ) {
+                            ItemList()
+                        }
+                    }
                 }
             }
         }
